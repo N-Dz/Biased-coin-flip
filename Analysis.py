ca1423d7e6afbccf5df1ba1e3d5e1e8c48cbbff3
N-Dz <103330430+N-Dz@users.noreply.github.com>
2022-04-22 15:13:37 +0300

    Add files via upload

---
+++ Analysis.py
@@ -0,0 +1,4 @@
+from Pseudo_monte_carlo import pseudo_simulation_plot
+
+# call the function; parameters: number og simulations, win p, loss p, bet size, balance, number of bets, risk, reward
+pseudo_simulation_plot(1000, 0.51, 0.49, 5, 100, 100, 1, 1)
