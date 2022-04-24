942318b4db48acc6dd6cb2135c75d07cb908bc55
N-Dz <103330430+N-Dz@users.noreply.github.com>
2022-04-24 23:48:29 +0300

    Update pseudo_monte_carlo.py

--- pseudo_monte_carlo.py
+++ pseudo_monte_carlo.py
@@ -1,7 +1,8 @@
-from simulation import Simulation
+from coin_flip import CoinFlip
 import matplotlib.pyplot as pyplot
 
 
+# simulates and plots multiple outcomes of the same biased coin flip strategy
 def simulate_and_plot(number_of_simulations, probability_win, probability_loss, bet_size, balance,
                            number_of_bets,
                            risk,
