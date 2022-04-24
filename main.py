6ee39fa81b9c81e7f06195bbe05dc14acaed5c97
N-Dz <103330430+N-Dz@users.noreply.github.com>
2022-04-24 23:50:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Pseudo_monte_carlo import pseudo_simulation_plot
+from Pseudo_monte_carlo import simulate_and_plot
 
 # call the function; parameters: number of simulations, win p, loss p, bet size(in units), balance(in units), number of bets, risk, reward
 simulate_and_plot(1000, 0.51, 0.49, 5, 100, 100, 1, 1)
