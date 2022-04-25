9ae0383987d289bb5afe43828522cd53dee66367
N-Dz <103330430+N-Dz@users.noreply.github.com>
2022-04-25 14:26:11 +0300

    Update pseudo_monte_carlo.py

--- pseudo_monte_carlo.py
+++ pseudo_monte_carlo.py
@@ -8,7 +8,7 @@ def p_monte_carlo(number_of_simulations, probability_win, probability_loss, bet_
                            risk,
                            reward):
     for x in range(number_of_simulations):
-        strategy = Simulation(probability_win, probability_loss, bet_size, balance,
+        strategy = CoinFlip(probability_win, probability_loss, bet_size, balance,
                               number_of_bets, risk, reward)
         strategy.flip()
         pyplot.plot(strategy.portfolio)
